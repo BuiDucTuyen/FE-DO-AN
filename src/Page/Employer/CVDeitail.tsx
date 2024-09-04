c40a5c05469c64d64ed25bdf726c36019e5d85e8
BuiDucTuyen <buiductuyengl1072002@gmail.com>
2024-09-05 00:38:36 +0700

    fix fe

--- src/Page/Employer/CVDeitail.tsx
+++ src/Page/Employer/CVDeitail.tsx
@@ -13,6 +13,7 @@ import Footer from "../../Components/Footter";
 const CvDetail: React.FC = () => {
   const { jobId } = useParams<{ jobId: string }>();
   const [listFiles, setListFiles] = useState<CVDetail[]>([]);
+  const [showTable, setShowTable] = useState(false);
 
   useEffect(() => {
     const getListFile = async () => {
@@ -35,9 +36,101 @@ const CvDetail: React.FC = () => {
         <h1 className="text-4xl font-bold mb-12 text-center text-gray-800">
           Danh sách CV
         </h1>
-        <h1 className="text-2xl font-semibold mb-6  text-gray-600">
-          Có {listFiles.length} CV ứng tuyển
+        <h1 className="text-2xl font-semibold mb-6 text-gray-600">
+          Có {listFiles.length} CV ứng tuyển.{" "}
+          <span
+            onClick={() => setShowTable(!showTable)}
+            className="text-blue-600 cursor-pointer">
+            Thống kê
+          </span>
         </h1>
+
+        {showTable && (
+          <div className="mt-5 mb-5 overflow-x-auto">
+            <h2 className="text-3xl font-bold mb-4 text-gray-700">
+              Bảng thống kê CV ứng tuyển
+            </h2>
+            <table className="min-w-full bg-white shadow-md rounded-lg">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    STT
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Tên ứng viên
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Địa chỉ
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Số điện thoại
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Ngành nghề
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Tuổi
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Trình độ
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Giới thiệu cá nhân
+                  </th>
+                  <th className="px-6 py-3 border-b-2 border-gray-200 bg-gray-50 text-left text-sm font-semibold text-gray-600 uppercase tracking-wider">
+                    Liên kết CV
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {listFiles.map((file, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {index + 1}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.name}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.employee_address.street} -{" "}
+                      {file.employee.employee_address.district} -{" "}
+                      {file.employee.employee_address.city}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.number}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.major.map((major, idx) => (
+                        <span key={idx} className="block">
+                          {major.name}
+                        </span>
+                      ))}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.age}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.level}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      {file.employee.personal_introduction}
+                    </td>
+                    <td className="px-6 py-4 border-b border-gray-200 text-sm">
+                      <a
+                        href={file.cv_url.toString()}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 hover:underline">
+                        Xem CV
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
         <div className="flex justify-center">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {listFiles.map((file, index) => (
@@ -102,7 +195,6 @@ const CvDetail: React.FC = () => {
           </div>
         </div>
       </div>
-
       <Footer />
     </section>
   );
